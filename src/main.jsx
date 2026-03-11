6af93a7923a927065b0c9136e73a074575d6da59
tailopez147 <49619133+tailopez147@users.noreply.github.com>
2026-03-11 19:07:33 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { useAuth } from './contexts/AuthContext'
-import './Page.css'
+import './pages/Page.css'
 
 const Mail = () => {
   const { access } = useAuth();
